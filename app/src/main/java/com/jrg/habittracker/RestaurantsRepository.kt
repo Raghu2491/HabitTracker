fea5aeabaec4eb7811d0f554519a6440af121f10
arjay <raghavendra0204@gmail.com>
2024-05-16 21:51:25 -0500

    - Separate Presentation logic from Data logic

---
+++ app/src/main/java/com/jrg/habittracker/RestaurantsRepository.kt
@@ -0,0 +1,51 @@
+package com.jrg.habittracker
+
+import com.jrg.habittracker.network.RestaurantsApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
+
+class RestaurantsRepository {
+    private val restInterface: RestaurantsApiService =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://restaurants-db-default-rtdb.firebaseio.com/")
+            .build()
+            .create(RestaurantsApiService::class.java)
+
+    private val restaurantsDao =
+        RestaurantsDB.getDaoInstance(RestaurantsApplication.getAppContext())
+
+    suspend fun getAllRestaurants(): List<Restaurant> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val restaurants = restInterface.getRestaurants()
+                if(restaurantsDao.getAll().isEmpty())
+                    restaurantsDao.addAll(restaurants)
+                val favoriteRestaurants = restaurantsDao.getAllFavorites()
+                restaurantsDao.updateAll(
+                    favoriteRestaurants.map {
+                        PartialRestaurant(it.id, isLiked = true)
+                    }
+                )
+                return@withContext restaurantsDao.getAll()
+
+            } catch (e: Exception) {
+                return@withContext restaurantsDao.getAll()
+            }
+        }
+    }
+
+    suspend fun toggleFavoriteRestaurant(id: Int, isLiked: Boolean) =
+        withContext(Dispatchers.IO) {
+            restaurantsDao.update(
+                PartialRestaurant(
+                    id,
+                    isLiked
+                )
+            )
+            restaurantsDao.getAll()
+        }
+}
--- app/src/main/java/com/jrg/habittracker/RestaurantsViewModel.kt
+++ app/src/main/java/com/jrg/habittracker/RestaurantsViewModel.kt
@@ -13,54 +13,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.Exception
 
-class RestaurantsViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
-    private var restInterface: RestaurantsApiService
-    val state = mutableStateOf(RestaurantsScreenState(
-        restaurants = listOf(),
-        isLoading = true
-    ))
-    private lateinit var restaurantsCall: Call<List<Restaurant>>
-    private var restaurantsDao =
-        RestaurantsDB.getDaoInstance(RestaurantsApplication.getAppContext())
+class RestaurantsViewModel : ViewModel() {
+    val state = mutableStateOf(
+        RestaurantsScreenState(
+            restaurants = listOf(),
+            isLoading = true
+        )
+    )
+
+    val repository = RestaurantsRepository()
 
     init {
-        val retrofit: Retrofit = Retrofit.Builder()
-            .addConverterFactory(
-                GsonConverterFactory.create()
-            )
-            .baseUrl(
-                "https://restaurants-db-default-rtdb.firebaseio.com/"
-            )
-            .build()
-        restInterface = retrofit.create(
-            RestaurantsApiService::class.java
-        )
         getRestaurants()
-    }
-
-    private suspend fun getAllRestaurants(): List<Restaurant> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val restaurants = restInterface.getRestaurants()
-                if(restaurantsDao.getAll().isEmpty())
-                    restaurantsDao.addAll(restaurants)
-                val favoriteRestaurants = restaurantsDao.getAllFavorites()
-                restaurantsDao.updateAll(
-                    favoriteRestaurants.map {
-                        PartialRestaurant(it.id, isLiked = true)
-                    }
-                )
-                return@withContext restaurantsDao.getAll()
-
-            } catch (e: Exception) {
-                return@withContext restaurantsDao.getAll()
-            }
-        }
     }
 
     fun getRestaurants() {
         viewModelScope.launch(Dispatchers.IO) {
-            val restaurants = getAllRestaurants()
+            val restaurants = repository.getAllRestaurants()
             state.value = state.value.copy(
                 restaurants = restaurants,
                 isLoading = false
@@ -73,55 +42,10 @@ class RestaurantsViewModel(private val savedStateHandle: SavedStateHandle) : Vie
         val itemIndex = restaurants.indexOfFirst { it.id == id }
         restaurants[itemIndex] =
             restaurants[itemIndex].copy(isLiked = !restaurants[itemIndex].isLiked)
-        saveSelection(restaurants[itemIndex])
         viewModelScope.launch {
-            val updatedRestaurants = toggleFavoriteRestaurant(id, restaurants[itemIndex].isLiked)
+            val updatedRestaurants =
+                repository.toggleFavoriteRestaurant(id, restaurants[itemIndex].isLiked)
             state.value = state.value.copy(restaurants = updatedRestaurants, isLoading = false)
         }
-    }
-
-    private suspend fun toggleFavoriteRestaurant(id: Int, isLiked: Boolean) =
-        withContext(Dispatchers.IO) {
-            restaurantsDao.update(
-                PartialRestaurant(
-                    id,
-                    isLiked
-                )
-            )
-            restaurantsDao.getAll()
-        }
-
-
-    private fun saveSelection(item: Restaurant) {
-        val savedInstance = savedStateHandle
-            .get<List<Int>?>(FAVORITE)
-            .orEmpty()
-            .toMutableList()
-        if (item.isLiked)
-            savedInstance.add(item.id)
-        else
-            savedInstance.remove(item.id)
-        savedStateHandle[FAVORITE] = savedInstance
-    }
-
-    private fun List<Restaurant>.restoreSelections(): List<Restaurant> {
-        savedStateHandle.get<List<Int>?>(FAVORITE)?.let { savedIDs ->
-            val restaurantMap = this.associateBy { it.id }.toMutableMap()
-            savedIDs.forEach { id ->
-                val restaurant = restaurantMap[id] ?: return@forEach
-                restaurantMap[id] = restaurant.copy(isLiked = true)
-            }
-            return restaurantMap.values.toList()
-        }
-        return this
-    }
-
-    companion object {
-        const val FAVORITE = "favorite"
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        restaurantsCall.cancel()
     }
 }
